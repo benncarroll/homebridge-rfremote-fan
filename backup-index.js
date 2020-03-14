4e7ed25807a513f2786aea1c4c8752f5c431f201
Ben Carroll <ben.andrew.carroll@gmail.com>
2020-03-14 11:44:23 +1100

    Big changes and improvements

--- backup-index.js
+++ backup-index.js
@@ -1,76 +1,74 @@
 var exec = require("child_process").exec;
+//var locks = require('locks');
 var request = require('request');
 
-var Service;
-var Characteristic;
+var Service, Characteristic;
 
 module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory('homebridge-rfremote-fan', 'RF-Remote Fan', FanLightAccessory);
+  homebridge.registerAccessory('homebridge-rfremote-fan', 'RF-Remote Fan', FanAccessory);
 };
 
-function FanLightAccessory(log, config) {
+function FanAccessory(log, config) {
   this.log = log;
 
   this.host = config.host;
   this.name = config.name;
   this.id = config.id;
-  this.light_name = config.light_name || this.name + " Light"
 
   this.state = {
     power: false,
-    speed: 25
+    speed: 25,
   };
+//  this.mutex = locks.createMutex()
 }
 
-FanLightAccessory.prototype.getRelays = function(value, callback) {
+FanAccessory.prototype.getRelays = function(value, callback) {
   request({
     url: 'http://' + this.host + '/rfremotefan/api/v1.0/status',
     method: 'GET',
     json: true,
-    body: value
+    body: value,
   }, function(error, response, body) {
     if (error) {
       callback(error);
     } else if (response.statusCode == 200) {
-      callback(null, body.on);
+      callback(null, body);
     } else {
       callback(new Error('HTTP response ' + response.statusCode + ': ' + JSON.stringify(body)));
     }
   });
 };
 
-FanLightAccessory.prototype.updateRelays = function(value, callback) {
+FanAccessory.prototype.updateRelays = function(value, callback) {
 
-  this.log('update relays: ' + JSON.stringify(value));
-
+  this.log('update relays: ' + value);
+	
   request({
     url: 'http://' + this.host + '/rfremotefan/api/v1.0/update',
     method: 'GET',
     json: true,
-    body: value
+    body: value,
   }, function(error, response, body) {
     if (error) {
       callback(error);
     } else if (response.statusCode == 200) {
-      callback(null, value);
+      callback(null);
     } else {
       callback(new Error('HTTP response ' + response.statusCode + ': ' + JSON.stringify(body)));
     }
   });
 };
 
-FanLightAccessory.prototype.getFanState = function(callback) {
-  info = {
-    "id": this.id
-  };
+FanAccessory.prototype.getFanState = function(callback) {
+  info = {"id": this.id}
   this.getRelays(info, (error, data) => {
     if (error) {
       callback(error);
     } else {
       var state = {}
-      speed = data["speed"];
+	  speed = data["speed"]
       if (speed == 3) {
         state.power = true;
         state.speed = 100;
@@ -85,14 +83,14 @@ FanLightAccessory.prototype.getFanState = function(callback) {
         state.speed = 25;
       }
       // state.temperature = data.temperature;
-      this.log("speed ---" + state.speed);
+	  this.log("speed ---" + state.speed)
       this.state = state;
       callback(null, state);
     }
   });
 };
 
-FanLightAccessory.prototype.setFanState = function(state, callback) {
+FanAccessory.prototype.setFanState = function(state, callback) {
   var relay;
   if (state.power && state.speed > 50) {
     relay = 3;
@@ -106,92 +104,80 @@ FanLightAccessory.prototype.setFanState = function(state, callback) {
 
   this.log('active relay ' + relay);
 
-  var update1 = {};
+  var update1 = {
+  };
   update1["id"] = this.id;
   update1["speed"] = relay;
 
-  this.updateRelays(update1, (error) => {
-    if (error) {
-      callback(error);
-      return;
-    } else {
-      callback()
-    }
-  });
-};
+  var update2 = {
+    1: false,
+    2: false,
+    3: false,
+  };
+  if (relay) {
+    delete update2[relay];
+  }
 
-FanLightAccessory.prototype.identify = function(callback) {
+  this.updateRelays(update1, (error) => {
+      if (error) {
+        callback(error);
+        return;
+      }
+  });
+
+}
+
+FanAccessory.prototype.identify = function(callback) {
   this.log("Identify requested!");
   callback();
 };
 
-FanLightAccessory.prototype.getServices = function() {
-
-  // Fan
+FanAccessory.prototype.getServices = function() {
   this.fanService = new Service.Fan();
   this.fanService.getCharacteristic(Characteristic.On)
-    .on('get', this.getFanOn.bind(this))
-    .on('set', this.setFanOn.bind(this));
+    .on('get', this.getOn.bind(this))
+    .on('set', this.setOn.bind(this));
   this.fanService.getCharacteristic(Characteristic.RotationSpeed)
     .setProps({
       minValue: 0,
       maxValue: 100,
-      minStep: 25
+      minStep: 25,
     })
-    .on('get', this.getFanSpeed.bind(this))
-    .on('set', this.setFanSpeed.bind(this));
+    .on('get', this.getSpeed.bind(this))
+    .on('set', this.setSpeed.bind(this));
 
-  // Light
-  this.lightService = new Service.Lightbulb(this.light_name);
-  this.lightService.getCharacteristic(Characteristic.On)
-    .on('get', this.getLightOn.bind(this))
-    .on('set', this.setLightOn.bind(this));
-
-  return [this.fanService, this.lightService];
+  return [this.fanService];
 };
 
-FanLightAccessory.prototype.getFanOn = function(callback) {
+FanAccessory.prototype.getOn = function(callback) {
   this.getFanState(function(error, state) {
-    callback(null, state.power);
+    callback(null, state && state.power);
   });
 };
 
-FanLightAccessory.prototype.setFanOn = function(value, callback) {
-  //if (this.state.power != value) {
+FanAccessory.prototype.setOn = function(value, callback) {
+  if (this.state.power != value) {
     this.log('setting power to ' + value);
     this.state.power = value;
     this.setFanState(this.state, callback);
-    callback();
+  } else {
+    callback(null);
+  }
 };
 
-FanLightAccessory.prototype.getFanSpeed = function(callback) {
+FanAccessory.prototype.getSpeed = function(callback) {
   this.log("get speed");
   this.getFanState(function(error, state) {
-    callback(null, state.speed);
+    callback(null, state && state.speed);
   });
 };
 
-FanLightAccessory.prototype.setFanSpeed = function(value, callback) {
-  //if (this.state.speed != value) {
+FanAccessory.prototype.setSpeed = function(value, callback) {
+  if (this.state.speed != value) {
     this.log('setting speed to ' + value);
     this.state.speed = value;
     this.setFanState(this.state, callback);
-    callback();
+  } else {
+    callback(null);
+  }
 };
-
-FanLightAccessory.prototype.getLightOn = function(callback) {
-  this.log.info('Check light')
-  this.getRelays({
-    id: this.id,
-    light: true
-  }, callback);
-};
-
-FanLightAccessory.prototype.setLightOn = function(newValue, callback) {
-  this.log.info('Light on: ' + newValue)
-  this.updateRelays({
-    id: this.id,
-    light: newValue
-  }, callback);
-};
-
